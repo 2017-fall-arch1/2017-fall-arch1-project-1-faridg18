ab2d1108b7cdc6ce9ac2a781d299dd9e165dabaf
student <student@localhost.localdomain>
2017-10-17 22:25:46 -0600

    99% there.

---
+++ .#PMS.c
@@ -0,0 +1,1 @@
+student@localhost.localdomain.1181:1508271232
--- PMS.c
+++ PMS.c
@@ -6,13 +6,14 @@
 #include "deleteEmployee.h"
 #include "listAllEmployees.h"
 
-
+//defining a node
 typedef struct Node{
   char *name;
   struct Node *left;
   struct Node *right;
-} Node;
+} Node; // name to call this structure
 
+// methods calls
 Node *createNode(char *Name);
 Node *insert (Node* nNode, Node* head);
 void displayTree(Node *head);
@@ -46,13 +47,15 @@ int main(){
   }
   printf("thank you for choosing the personal management system 2017.\n");
   return 0;
-} 
+}
+
+//insert employee into tree
 void addEmployee(FILE *X){
   char in[50];
   X = fopen("test.txt", "a");
   printf("\tenter employee name\n");  
   scanf("%s", &in);
-  fputs(strcat(in, "\n"), X);
+  fputs(strcat(in, "\n"), X); //append string into the last line in the file
   fclose(X);
   printf("%s", "do you want to add another one?(1, 0)\n");
   int choice;
@@ -63,6 +66,7 @@ void addEmployee(FILE *X){
   
 }
 
+//remove name from text file
 void deleteEmployee(FILE *X){
   char temp[50];
   X = fopen("test.txt","r");
@@ -72,19 +76,20 @@ void deleteEmployee(FILE *X){
   Y = fopen("test2.txt", "a");
   scanf("%s", &search_name);
   strcat(search_name, "\n");
- 
+  //traverse the file and copy it to another
   while(fgets(temp, sizeof(temp), X)){
-    if(strcmp(temp, search_name) != 0){
+    if(strcmp(temp, search_name) != 0){//if the string is met
       fputs(temp, Y);
     }
   }
   fclose(X);
   fclose(Y);
-  remove("test.txt");
+  remove("test.txt"); //delete the previous file
   
-  rename("test2.txt", "test.txt");
+  rename("test2.txt", "test.txt"); // rename new file
 }
 
+//read a file and display the tree
 void listAllEmployees(FILE *X){
   char temp[50];
   int id = 1;
@@ -121,6 +126,8 @@ void listAllEmployees(FILE *X){
   free(root);
   fclose(X);
 }
+
+//display the tree in order
 void displayTree(Node * head){
   if(head == NULL || head -> name == NULL){
     return;
@@ -129,30 +136,34 @@ void displayTree(Node * head){
   printf("%s", head->name);
   displayTree(head -> right);
 }
+
+//insert nodes into tree
 Node *insert(Node *nNode, Node *head){
-  if(!head){
+  if(!head){ // when head is empty
     head = nNode;
   }
-  if(strcmp(nNode -> name, head -> name) < 0){
-   if(head -> left){
+  if(strcmp(nNode -> name, head -> name) < 0){ // if needs to move to the left
+    if(head -> left){ // if the node is taken
      head -> left = insert(nNode, head -> left);
    }
-   else{
+    else{//when the node is free
      head -> left = nNode;
      return head;
    }
   }
-  else if(strcmp(nNode -> name, head -> name) > 0){
-    if(head -> right){
+  else if(strcmp(nNode -> name, head -> name) > 0){//when it needs to traverse to the right
+    if(head -> right){ // when node is taken
       head -> right = insert (nNode, head->right);
     }
-    else{
+    else{//when node is available
       head -> right = nNode;
       return head;
     }
   }
   return head;
 }
+
+//allocate memory for the node and set the name
 Node *createNode(char *Name){
   Node *newNode;
   newNode = malloc(sizeof(Node));
